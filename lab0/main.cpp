df736c42cec5e45f00597f84aaa6ee106340793d
Kiril Vuchkov <kirilvuchkov@gmail.com>
2017-02-27 18:55:38 +0200

    lab0 solved

--- lab0/main.cpp
+++ lab0/main.cpp
@@ -3,6 +3,34 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    string names[n];
+    int mat[n][n] = {0}; // all zeroes
+
+    for (int i = 0; i < n; i++)
+        cin >> names[i];
+
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        mat[u][v] = 1; // u is a friend of v
+        mat[v][u] = 1; // and v is a friend of u
+    }
+
+    int q;
+    while (cin >> q) { //list the friends if q
+        cout << names[q] << ":"; // print the name of q
+
+        for (int i = 0; i < n; i++) {
+            if (mat[q][i]) // then i is a friend of q
+                cout << " " << names[i];
+        }
+        cout << endl;
+    }
 
     return 0;
 }
