a65e4e47b40c713f8855076e9bd6d34e8522caf3
Kiril Vuchkov <kirilvuchkov@gmail.com>
2017-05-01 22:47:58 +0300

    lab9 solved

--- lab9/src/ex1.cpp
+++ lab9/src/ex1.cpp
@@ -4,6 +4,33 @@
 
 using namespace std;
 
+bool valid(string expr);
+
+
 int main(int argc, char *argv[]) {
+    string line;
+    while (getline(cin, line)) {
+        cout << (valid(line) ? "YES" : "NO") << endl;
+    }
     return 0;
+}
+
+inline char opening(char c) {
+    return c == ')' ? '(' : '[';
+}
+
+bool valid(string expr) {
+    stack<char> brackets;
+    for (int i = 0; i < expr.length(); i++) {
+        char c = expr[i];
+        if (c == '(' or c == '[')
+            brackets.push(c);
+        else if (c == ')' or c == ']') {
+            if (brackets.top() == opening(c)) {
+                brackets.pop();
+            } else
+                return false;
+        }
+    }
+    return brackets.empty();
 }
--- lab9/src/ex2.cpp
+++ lab9/src/ex2.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <stack>
 #include <algorithm>
 #include <fstream>
 #include <map>
 
 using namespace std;
 
+bool valid(string expr);
+
+
 int main(int argc, char *argv[]) {
+    if (argc < 2)
+        return -1;
+
+    ifstream fin(argv[1]);
+    if (fin.fail())
+        return -2;
+
+    map<string, int> m;
+
+    string word;
+    while (fin >> word) {
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        m[word] = m[word] + 1;
+    }
+
+    map<string, int>::iterator best = m.begin();
+    for (auto it = m.begin(); it != m.end(); it++) {
+        if (best->second < it->second)
+            best = it;
+    }
+
+    cout << best->first << " " << best->second << endl;
 
     return 0;
 }
