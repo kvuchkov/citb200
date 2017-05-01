c4c67103fcf0d1a8c789b05a0220afbfac166220
Kiril Vuchkov <kirilvuchkov@gmail.com>
2017-05-01 22:46:30 +0300

    lab9 prepared

---
+++ lab9/src/ex1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
---
+++ lab9/src/ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
