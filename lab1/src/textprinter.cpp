96caf827650514631242685222f7ab6e69111d88
Kiril Vuchkov <kirilvuchkov@gmail.com>
2019-04-07 13:37:13 +0300

    Format doubles with precision 2

--- lab1/src/textprinter.cpp
+++ lab1/src/textprinter.cpp
@@ -1,8 +1,11 @@
 #include "textprinter.h"
+#include <iomanip>
 
 using std::endl;
 
 void TextPrinter::print(std::ostream &out, Invoice invoice) {
+	out << std::fixed << std::setprecision(2);
+	
 	for (auto item : invoice.getItems()) {
 		out << item.getQuantity() << " x " 
 			<< item.getProduct().getName() 
