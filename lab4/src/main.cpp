4ab7efd44733ff54f2a0ea5f34fd8b8036134e50
Kiril Vuchkov <kirilvuchkov@gmail.com>
2019-05-02 10:44:08 +0300

    File Streams - Step 2 - Read input file

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -13,7 +13,28 @@ using std::ifstream;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::cerr;
+using std::string;
 
-int main() {
+int main(int argc, char const *argv[])
+{
+    if (argc != 2) {
+        cerr << "Invalid usage! Expected:" << endl << "./lab4 /path/to/input" << endl;
+        return -1;
+    }
+
+    string inputsPath = argv[1];
+
+    cerr << "Reading shapes from " << inputsPath << "..." << endl;
+
+    ifstream inputs(inputsPath);
+
+    string word;
+    while (inputs >> word) {
+        cout << word << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
