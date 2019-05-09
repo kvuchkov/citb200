f4d80cd733ca6053dcf98db7801d12c502960069
Kiril Vuchkov <kirilvuchkov@gmail.com>
2019-05-09 10:46:56 +0300

    File Streams - Step 14 - Define read in Triangle

--- lab4/src/triangle.cpp
+++ lab4/src/triangle.cpp
@@ -22,3 +22,19 @@ double Triangle::perimeter()
 {
 	return a + b + c;
 }
+
+bool Triangle::read(std::istream &in)
+{
+	int a, b, c;
+	if (in >> a >> b >> c)
+	{
+		if (a < b + c && b < a + c && c < a + b)
+		{
+			this->a = a;
+			this->b = b;
+			this->c = c;
+			return true;
+		}
+	}
+	return false;
+}
